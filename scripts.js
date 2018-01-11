daebae42823858e85857f01e8af92d198fb6e749
Gergely Virágh <vghgergely@gmail.com>
2018-01-11 14:46:26 +0100

    Started styling, no menus yet

--- scripts.js
+++ scripts.js
@@ -3,13 +3,17 @@
     // false: player true: computer
     var player = "";
     var comp = "";
+    // these 2 should be <i fa> things and text should be innerHTML instead, hopefully display is not getting fucked up, the table is perfect as it is
+    var playing = false;
     var table = [];
 
     
     function checkIfWon() {
         for (i = 0; i < table.length; i++) {
-            if ((table[i][0] != "" ) && (table[i][0] == table[i][1]) && (table[i][1] == table[i][2])) {
+            if ((table[i][0] != "") && (table[i][0] == table[i][1]) && (table[i][1] == table[i][2])) {
+                
                 alert("win procced horizontal, line: " + table[i][0]);
+                
                 $("#reset").trigger("click");
             }
             if ((table[0][i] != "") && (table[0][i] == table[1][i]) && (table[1][i] == table[2][i])) {
@@ -50,15 +54,17 @@
         //SOME GLOWING AFTER WINNING AND STUFF SO IT LOOKS NICE
     }
 
-    $("#start").click(function() {
+    $("#start").click(function () {
+        $(".grid").show();
         $(".chooser").toggle();
         table = [["", "", ""], ["", "", ""], ["", "", ""]];
+        
     })
 
     $("#reset").click(function() {
         table = [["", "", ""], ["", "", ""], ["", "", ""]];
         $(".cell").text("");
-        
+        playing = false;
     });
 
     $(".chooser").click(function() {
@@ -72,34 +78,39 @@
         $("#player").text("player: " + player);
         $("#comp").text("comp: " + comp);
         $(".chooser").toggle();
+        playing = true;
     })
     
         
     
 
-    $(".cell").click(function() {
-        var cell = $(this);
-        if (cell.text() == "") {
+    $(".cell").click(function () {
+        if(playing){
+            var cell = $(this);
+            if (cell.text() == "") {
             
-            var coords = this.id.toString();
-            console.log(coords);
-            var i = parseInt(coords[0]);
-            var j = parseInt(coords[1]);
-            console.log("i: " + i + " j: " + j);
-            console.log(table[i][j]);
-            if (currTurn) {
-                table[i][j] = player;
-                cell.text(player);
-            
-            }
+                var coords = this.id.toString();
+                console.log(coords);
+                var i = parseInt(coords[0]);
+                var j = parseInt(coords[1]);
+                console.log("i: " + i + " j: " + j);
+                console.log(table[i][j]);
+                if (currTurn) {
+                    table[i][j] = player;
+                    cell.text(player);
+                    //setTimeout(function() { alert("meme");
+                    //    reset();
+                    //}, 1000);
+                }
            
 
-            console.log(table);
-            checkIfWon();
-            currTurn = !currTurn;
-            getNextMove();
-            console.log(table);
+                console.log(table);
+                checkIfWon();
+                currTurn = !currTurn;
+                getNextMove();
+                console.log(table);
             
+            }
         }
     })
 })
